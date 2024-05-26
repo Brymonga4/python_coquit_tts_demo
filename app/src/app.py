e0c4d160309c802b19771be49b3d464711be1542
Brymonga4 <bryanmoises.montesdeoca@alumno.iesluisvives.org>
2024-05-26 20:20:37 +0200

    Added a few test for existing models

---
+++ app/src/app.py
@@ -0,0 +1,44 @@
+import os
+import torch
+from services.util import Utilities
+from services.examples import Models_Examples
+
+
+CURRENT_EXECT_PATH = os.getcwd()
+TEST_FILES_PATH = os.path.join(CURRENT_EXECT_PATH,"app","src", "samples")
+OUTPUT_FILES_PATH = os.path.join(CURRENT_EXECT_PATH,"app","src", "output")
+
+# List available 🐸TTS models
+# print(TTS().list_models())
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+text = Utilities.read_text_file(os.path.join(TEST_FILES_PATH, "text", "eng_sample.txt"))
+
+
+# Text to speech to a file
+# ['en', 'es', 'fr', 'de', 'it', 'pt', 'pl', 'tr', 'ru', 'nl', 'cs', 'ar', 'zh-cn', 'hu', 'ko', 'ja', 'hi']
+Models_Examples.xtts_from_sample_audio_and_text_to_output_audio(device,
+                                                                text, 
+                                                                os.path.join(TEST_FILES_PATH, "donquixote_my_voice_spa.wav"),
+                                                                "en",
+                                                                os.path.join(OUTPUT_FILES_PATH, "output_xtts_v2.wav")
+                                                                )
+
+Models_Examples.your_tts_from_sample_audio_and_text_to_output_audio(device,
+                                                                text, 
+                                                                os.path.join(TEST_FILES_PATH, "donquixote_my_voice_spa.wav"),
+                                                                "en",
+                                                                os.path.join(OUTPUT_FILES_PATH, "output_your_tts.wav")
+                                                                )
+
+Models_Examples.vctk_from_audio_to_audio_output(device,
+                                                os.path.join(TEST_FILES_PATH, "donquixote_my_voice_spa.wav"),
+                                                os.path.join(TEST_FILES_PATH, "leonor.wav"),
+                                                os.path.join(OUTPUT_FILES_PATH, "output_vctk.wav")
+                                                )
+
+Models_Examples.tacotron2_GER_from_text_to_output_audio(device,
+                                                    "Ich bin eine Testnachricht.",
+                                                    os.path.join(OUTPUT_FILES_PATH, "output_GER.wav")
+                                                    )
---
+++ app/src/services/examples.py
@@ -0,0 +1,43 @@
+from TTS.api import TTS
+
+
+class Models_Examples:
+
+    @staticmethod
+    def xtts_from_sample_audio_and_text_to_output_audio(device, text, audio_sample_path, lang, output_path):
+        tts = TTS("tts_models/multilingual/multi-dataset/xtts_v2").to(device)
+        tts.tts_to_file(text=text, 
+                speaker_wav=audio_sample_path, 
+                language=lang, 
+                file_path=output_path,
+                split_sentences=True)
+        
+    @staticmethod
+    def vctk_from_audio_to_audio_output(device, source_sample_path, target_sample_path, output_path):
+        tts = TTS(model_name="voice_conversion_models/multilingual/vctk/freevc24", progress_bar=False).to(device)
+        tts.voice_conversion_to_file(source_wav=source_sample_path,
+                                    target_wav=target_sample_path, 
+                                    file_path=output_path)     
+    
+    @staticmethod
+    def tacotron2_GER_from_text_to_output_audio(device, text, output_path):
+        tts = TTS(model_name="tts_models/de/thorsten/tacotron2-DDC", progress_bar=False).to(device)
+        tts.tts_to_file(text=text, 
+                        file_path=output_path)
+        
+    @staticmethod
+    def your_tts_from_sample_audio_and_text_to_output_audio(device, text, audio_sample_path, lang, output_path):
+        tts = TTS(model_name="tts_models/multilingual/multi-dataset/your_tts", progress_bar=False).to(device)
+        tts.tts_to_file(text=text, 
+                speaker_wav=audio_sample_path, 
+                language=lang, 
+                file_path=output_path,
+                split_sentences=True)
+    @staticmethod
+    def fairseq_from_sample_audio_and_text_to_output_audio(text, audio_sample_path, lang, output_path):
+        # https://github.com/facebookresearch/fairseq/tree/main/examples/mms/tts/tutorial
+        api = TTS(f"tts_models/{lang}/fairseq/vits")
+        api.tts_with_vc_to_file(
+            text,
+            speaker_wav=audio_sample_path,
+            file_path=output_path )     
